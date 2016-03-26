9b0edc62a9de71cda9d680fa5fe8075cdef05be5
mctavish141 <Serega@MacBook-Air.local>
2016-03-26 23:24:10 +0200

    v1.1

--- Thread/Thread/main.c
+++ Thread/Thread/main.c
@@ -9,19 +9,27 @@
 #include <stdio.h>
 #include <pthread.h>
 
+// sat_hello function
+
 void *say_hello (void *data) {
     printf("I am function say_hello\n");
     
     return 0;
 }
 
+// Main function
+
 int main() {
+    // Create thread
     pthread_t thread;
     
+    // Thread will run 'say_hello' function
     pthread_create(&thread, NULL, say_hello, NULL);
     
+    // Main continues
     printf("I am main\n");
     
+    // Wait until 'say_hello' will exit (СЮДА ПОКА МОЖЕШЬ НЕ СМОТРЕТЬ)
     pthread_join(thread, NULL);
     
     return 0;
